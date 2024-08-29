82aa4768bb951d26ee8dd3a42ddc040d20988d80
asdru0510 <sifontesupta@gmail.com>
2024-08-28 20:01:29 -0500

    Se agregan modelos y se migran a la DB

--- arriendos/urls.py
+++ arriendos/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+		path('admin/', admin.site.urls),
+		path('', include('main.urls')), # En este caso, la app se llama main
+		]
+
+
